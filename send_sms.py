1764e96214722647f5370e963fb28eb5c5710439
sourav-PC\sourav <shourav6688@gmail.com>
2018-06-25 20:21:56 +0600

    Updated sendsms.py

--- send_sms.py
+++ send_sms.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 
-account = "AC6ac96158bda1b5d814035373b8b64329"
-token = "f761d8b7a336aa746dbb533053a0ae74"
+account = "Your_account"
+token = "Your_account_token"
 client = Client(account, token)
-num=['+8801674206688','+8801937261969']
+num=['any phone number','any phone number']
 
 for i in range(0,len(num)):
-    message = client.messages.create(num[i], from_="+16677716206",
+    message = client.messages.create(num[i], from_="your twilo phone number",
                                  body="Hello")
